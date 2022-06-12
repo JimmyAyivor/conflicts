9e105db9530cd06b457938e9baae66643ba9d7e6
Jimmy Ayivor <jimmyayivor@gmail.com>
2022-06-12 12:18:20 -0400

    adding function7

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-
-
-function fn(...apples) {
-  if (!apples.every((apple) => Number.isNumber(apple)))
-    throw "All arguments must be numbers.";
-  return apples.reduce((numA, numB) => numA + numB);
-}
-
-fn(1, 2, 3); //> 6 ( 1+2+3)
-fn(10, "B", 20); //> error All arguments must be numbers
-//Can not be letters
+function fn(...c) {
+    if (!c.every((a) => Number.isNumber(a)))
+      throw "All arguments must be numbers.";
+    return c.reduce((a, b) => a + b);
+  }
+  
+  fn(1, 2, 3); //> 6
+  fn(10, "B", 20); //> error All arguments must be numbers.
