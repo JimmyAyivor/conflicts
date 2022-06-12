ba4fe10fbb75db7ae0e9ab41fde5165c1e57456e
Jimmy Ayivor <jimmyayivor@gmail.com>
2022-06-12 11:23:37 -0400

    adding function

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-function createPerson(name, age) {
-    return {
-      name,
-      age,
-      friends: [],
-    };
+function fn(...c) {
+    if (!c.every((a) => Number.isNumber(a)))
+      throw "All arguments must be numbers.";
+    return c.reduce((a, b) => a + b);
   }
   
-  function addPersonAsFriend(person, other) {
-    person.friends.push(other);
-    return person;
-  }
-  
-  module.exports = {
-    createPerson,
-    addPersonAsFriend,
-  };
+  fn(1, 2, 3); //> 6
+  fn(10, "B", 20); //> error All arguments must be numbers.
