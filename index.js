65d4ecece2021052969a794782346f83ac71d3cf
King Ankoue <kingankoue@Kings-Air.home>
2022-06-12 12:45:46 -0400

    newest

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function fn(...c) {
-    if (c.every((a) => Number.isNumber(a)))
+    if (!c.every((a) => Number.isNumber(a)))
       throw "All arguments must be numbers.";
     return c.reduce((a, b) => a - b);
   }
