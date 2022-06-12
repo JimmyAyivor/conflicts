9afeaa6947514733cabb0110a65e1ef68e8e86fb
Samantha Figueroa <samanthafigueroa@pursuit.org>
2022-06-12 11:53:37 -0400

    refactored function

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-
-
-function fn(...nums) {
-  if (!nums.every((num) => Number.isNumber(num)))
-    throw "All arguments must be numbers.";
-  return nums.reduce((numA, numB) => numA + numB);
+const fn = (...nums) => {
+  return !nums.every((num) => Number.isNumber(num)) ? "All arguments must be numbers." : nums.reduce((numA, numB) => numA + numB);
+  // if (!nums.every((num) => Number.isNumber(num)))
+  //   throw "All arguments must be numbers.";
+  // return nums.reduce((numA, numB) => numA + numB);
 }
 
 fn(1, 2, 3); //> 6
